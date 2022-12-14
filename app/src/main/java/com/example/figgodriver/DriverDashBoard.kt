c02b4728056db7c11280ce952474123ba8b9aa9a
Pearl <sudhanshu@pearlorganisation.com>
2022-12-14 15:24:34 +0530

    Ride Share and Ride Sale screens in Partner

--- app/src/main/java/com/example/figgodriver/DriverDashBoard.kt
+++ app/src/main/java/com/example/figgodriver/DriverDashBoard.kt
@@ -1,9 +1,13 @@
 package com.example.figgodriver
 
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -13,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DriverDashBoard : AppCompatActivity() {
     private lateinit var navController: NavController
+    var doubleBackToExitPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_dash_board)
@@ -23,5 +28,30 @@ class DriverDashBoard : AppCompatActivity() {
         navController=navHostFragment.navController
         val bottomNavigationView=findViewById<BottomNavigationView>(R.id.navigation_bar)
         setupWithNavController(bottomNavigationView,navController)
+
     }
+
+    override fun onBackPressed() {
+        val count = supportFragmentManager.backStackEntryCount
+        if (count == 0) {
+            if (doubleBackToExitPressedOnce) {
+                // System.exit(0);
+                val a = Intent(Intent.ACTION_MAIN)
+                a.addCategory(Intent.CATEGORY_HOME)
+                a.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(a)
+                finish()
+                return
+            }
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper()).postDelayed(Runnable {
+                doubleBackToExitPressedOnce = false
+            }, 2000)
+        } else {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
 }
