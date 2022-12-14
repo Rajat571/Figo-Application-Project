ce08e9adc394a7ad0d7d7621b59d97e5ef0d0b5f
Pearl Organisation <rajat@pearlorganisation.com>
2022-12-14 16:06:58 +0530

    MPIN Change

--- app/src/main/java/com/example/figgodriver/WelcomeActivity.kt
+++ app/src/main/java/com/example/figgodriver/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.TextView
 
 class WelcomeActivity : AppCompatActivity() {
@@ -12,9 +13,12 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_welcome)
         val window=window
         window.setStatusBarColor(Color.parseColor("#000F3B"))
-        var next_btn=findViewById<TextView>(R.id.next_button)
+        Handler().postDelayed({
+            startActivity(Intent(this,ChooseUserActivity::class.java))
+        },3000)
+      /*  var next_btn=findViewById<TextView>(R.id.next_button)
         next_btn.setOnClickListener {
             startActivity(Intent(this,ChooseUserActivity::class.java))
-        }
+        }*/
     }
 }
