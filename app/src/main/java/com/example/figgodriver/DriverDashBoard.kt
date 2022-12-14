71b3dfe2145e68919a40bb3dfb06c387c5988ef7
Pearl Organisation <rajat@pearlorganisation.com>
2022-12-14 15:19:02 +0530

    MPIN Change

--- app/src/main/java/com/example/figgodriver/DriverDashBoard.kt
+++ app/src/main/java/com/example/figgodriver/DriverDashBoard.kt
@@ -1,9 +1,13 @@
 package com.example.figgodriver
 
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -12,6 +16,8 @@ import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DriverDashBoard : AppCompatActivity() {
+
+    var doubleBackToExitPressedOnce = false
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,4 +30,30 @@ class DriverDashBoard : AppCompatActivity() {
         val bottomNavigationView=findViewById<BottomNavigationView>(R.id.navigation_bar)
         setupWithNavController(bottomNavigationView,navController)
     }
+
+    override fun onBackPressed() {
+        val count = supportFragmentManager.backStackEntryCount
+        if (count == 0) {
+            if (doubleBackToExitPressedOnce) {
+                // System.exit(0);
+                val a = Intent(Intent.ACTION_MAIN)
+                a.addCategory(Intent.CATEGORY_HOME)
+                a.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(a)
+                finish()
+                return
+            }
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper()).postDelayed(Runnable {
+                doubleBackToExitPressedOnce = false
+            }, 2000)
+        } else {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+
+
 }
