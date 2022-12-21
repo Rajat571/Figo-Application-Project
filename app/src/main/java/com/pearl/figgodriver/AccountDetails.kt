0676af3f88988eeb77644fdeeaaccb7057106807
madhuri <madhuri@pearlorganisation.com>
2022-12-21 17:43:07 +0530

    ON/OFF button

--- app/src/main/java/com/pearl/figgodriver/AccountDetails.kt
+++ app/src/main/java/com/pearl/figgodriver/AccountDetails.kt
@@ -1,10 +1,18 @@
 package com.pearl.figgodriver
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 //import com.example.figgodriver.Fragment.AccountDetailsFragment
 import com.pearl.figgodriver.R
+import kotlinx.android.synthetic.main.active_ride_layout.*
+import kotlinx.android.synthetic.main.fragment_driver_cab_details.view.*
+import kotlinx.android.synthetic.main.top_layout.view.*
 
 class AccountDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +20,19 @@ class AccountDetails : AppCompatActivity() {
         setContentView(R.layout.activity_account_details)
 
         var frame = findViewById<FrameLayout>(R.id.account_detailsFrame)
+        var topLayout = findViewById<LinearLayout>(R.id.layout)
+        var off_toggle = topLayout.toggle_off
+        var on_toggle = topLayout.toggle_on
+//        topLayout.setOnTouchListener { , motionEvent ->  }
+        topLayout.toggle_off.setOnClickListener {
+            off_toggle.setBackgroundColor(Color.RED)
+            on_toggle.setBackgroundColor(Color.WHITE)
+            Toast.makeText(this,"ASASAS",Toast.LENGTH_SHORT).show()
+        }
+        topLayout.toggle_on.setOnClickListener {
+            on_toggle.setBackgroundColor(Color.GREEN)
+            off_toggle.setBackgroundColor(Color.WHITE)
+        }
 //        var ad = AccountDetailsFragment()
 //        supportFragmentManager.beginTransaction().add(R.id.account_detailsFrame,ad).commit()
     }
