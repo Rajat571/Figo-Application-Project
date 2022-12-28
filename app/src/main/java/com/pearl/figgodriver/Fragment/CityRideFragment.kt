e3a96ce4823be04e208eb61e5fe720355d3ebfb7
madhuri <madhuri@pearlorganisation.com>
2022-12-28 17:47:13 +0530

    city ride layout created

--- app/src/main/java/com/pearl/figgodriver/Fragment/CityRideFragment.kt
+++ app/src/main/java/com/pearl/figgodriver/Fragment/CityRideFragment.kt
@@ -1,16 +1,29 @@
 package com.pearl.figgodriver.Fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.pearl.figgodriver.R
 import com.pearlorganisation.PrefManager
 
@@ -18,6 +31,10 @@ import com.pearlorganisation.PrefManager
 class CityRideFragment : Fragment(),OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     lateinit var prefManager: PrefManager
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    var lat:Double = 0.0
+    var long:Double = 0.0
+    var mCurrLocationMarker: Marker? = null
 
 
     override fun onCreateView(
@@ -34,19 +51,67 @@ class CityRideFragment : Fragment(),OnMapReadyCallback {
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+            override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+            override fun isCancellationRequested() = false
+        })
+            .addOnSuccessListener { location: Location? ->
+                if (location == null)
+                    Toast.makeText(requireContext(), "Cannot get location.", Toast.LENGTH_SHORT).show()
+                else {
+                    lat = location.latitude
+                    prefManager.setlatitude(lat.toFloat())
+                    long = location.longitude
+                    prefManager.setlongitude(long.toFloat())
+                    Toast.makeText(requireContext(),"Lat :"+lat+"\nLong: "+long, Toast.LENGTH_SHORT).show()
+                }
+
+            }
     }
 
     override fun onMapReady( googleMap: GoogleMap) {
+
         mMap = googleMap
 
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(prefManager.getlatitude().toDouble(), prefManager.getlongitude().toDouble())
-        mMap.addMarker(
+      //  val sydney = LatLng(prefManager.getlatitude().toDouble(), prefManager.getlongitude().toDouble())
+        val sydney=LatLng(prefManager.getlatitude().toDouble(),prefManager.getlongitude().toDouble())
+        mMap.clear()
+        mCurrLocationMarker =   mMap.addMarker(
             MarkerOptions()
             .position(sydney)
-            .title("Marker in Sydney"))
+            .title("Marker in Uttarakhand"))
+        val markerOptions = MarkerOptions()
+        // mMap.addMarker(markerOptions);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,18f))
     }
+
+
+
 
 
 }
