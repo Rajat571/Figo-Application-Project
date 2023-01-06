edf862b8a3dc71e162f2af573e9a794e52d09020
madhuri <madhuri@pearlorganisation.com>
2023-01-06 13:44:48 +0530

    upcoming Customer booking details API

--- app/src/main/java/com/pearl/figgodriver/Fragment/VerifyNumber.kt
+++ app/src/main/java/com/pearl/figgodriver/Fragment/VerifyNumber.kt
@@ -301,6 +301,9 @@ class VerifyNumber : Fragment(),GoogleApiClient.OnConnectionFailedListener  {
         }
     }
 
+
+
+
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
